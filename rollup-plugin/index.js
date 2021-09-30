5a86e5f030cb1179daad7cea6e53381a8ec98ee8
Adam Avramov <adam@hack.bg>
2021-09-30 14:12:02 +0300

    feat: transpile TS in rollup plugin

--- rollup-plugin/index.js
+++ rollup-plugin/index.js
@@ -1,16 +1,43 @@
-import { parseString } from '@hackbg/ganesha/parse.cjs'
+const { fileURLToPath } = require('url')
+const { parseString } = require('@hackbg/ganesha/parse.cjs')
 
 const RE_LITERATE = /\.(?:ts|js|cjs|mjs).md$/
     , isLiterate  = x => RE_LITERATE.test(x)
+const RE_TYPESCRIPT = /\.ts.md$/
+    , isTypescript  = x => RE_TYPESCRIPT.test(x)
+const isWindows = process.platform === "win32"
 
 export default function ganeshaPlugin () {
   return {
+
     name: 'rollup-plugin-ganesha',
-    transform (src, id) {
-      if (isLiterate(id)) return {
-        code: parseString(src),
-        map:  null
+
+    resolveId (source) {
+      console.log('resolveId', source)
+      return null
+    },
+
+    transform (code, id) {
+      console.log('TRANSFORM', id, isLiterate(id))
+      if (isLiterate(id)) {
+        code = parseString(code)
+        let map = null
+        if (isTypescript(id)){
+          const { transformSync } = require('esbuild')
+          const compiled = transformSync(code, {
+            sourcefile: id,
+            sourcemap: 'both',
+            loader: 'ts',
+            target: 'esnext',
+            format: 'esm' //format === 'module' ? 'esm' : 'cjs'
+          })
+          code = compiled.code
+          map = compiled.map
+        }
+        console.log(code)
+        return { code, map }
       }
     }
+
   }
 }
