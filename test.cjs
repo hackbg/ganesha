6984b1a343269cf770c72b86b1fa33c3d5243772
Adam Avramov <adam.b.avramov@gmail.com>
2021-12-03 13:22:10 +0100

    test: fix tests (but one)

--- test.cjs
+++ test.cjs
@@ -3,7 +3,7 @@ const { readFileSync } = require('fs')
 const { spawnSync } = require('child_process')
 const { resolve: resolvePath } = require('path')
 const { pathToFileURL } = require('url')
-const { parseFile } = require('./parse.cjs')
+const { parseFile } = require('./loader/parse.cjs')
 
 test('Parser: extract CommonJS module from Markdown, preserving line numbers', async ({same})=>{
   same(parseFile('examples/example.cjs.md'), readFileSync('examples/example.cjs', 'utf8')) })
@@ -47,7 +47,7 @@ test('CJS loader: identify literate CJS from front matter', async({same})=>{
 test('ESM loader: identify literate ESM from front matter', async({same})=>{
   const script = `import('./examples/example_mjs.md').then(code=>process.exit(code.default))`
       , args = [ '--unhandled-rejections=throw'
-               , '--experimental-loader', './loader.mjs'
+               , '--experimental-loader', '@hackbg/ganesha/loader.mjs'
                , '--input-type=module', '-e', script]
       , {status} = spawnSync('node', args, { stdio: 'inherit' })
   same(status, 123) })
@@ -55,13 +55,13 @@ test('ESM loader: identify literate ESM from front matter', async({same})=>{
 test('ESM loader: identify literate TS from front matter', async({same})=>{
   const script = `import('./examples/example_ts.md').then(code=>process.exit(code.default))`
       , args = [ '--unhandled-rejections=throw'
-               , '--experimental-loader', './loader.mjs'
+               , '--experimental-loader', '@hackbg/ganesha/loader.mjs'
                , '--input-type=module', '-e', script]
       , {status} = spawnSync('node', args, { stdio: 'inherit' })
   same(status, 123) })
 
 test('prioritize file import over directory import', async({same})=>{
-  await import('./loader.mjs').then(({ resolve })=>{
+  await import('./loader/loader.mjs').then(({ resolve })=>{
     same(resolve('./examples/example'), {
       url: pathToFileURL(resolvePath(__dirname, 'examples', 'example.ts')).toString() }) }) })
 
