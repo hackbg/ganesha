6948401faa3e38d5917bd9797978dc2aec3c0b54
Adam A <adam@hack.bg>
2021-12-25 00:16:39 +0200

    feat(rollup-plugin): support imports without extensions

--- rollup-plugin/index.js
+++ rollup-plugin/index.js
@@ -1,7 +1,9 @@
 const { fileURLToPath } = require('url')
+const { existsSync } = require('fs')
 const { parseString } = require('@hackbg/ganesha-core/parse.cjs')
+const { resolve, dirname } = require('path')
 
-const RE_LITERATE = /\.(?:ts|js|cjs|mjs).md$/
+const RE_LITERATE = /\.md$/
 const isLiterate  = x => RE_LITERATE.test(x)
 const RE_TYPESCRIPT = /\.ts.md$/
 const isTypescript  = x => RE_TYPESCRIPT.test(x)
@@ -12,7 +14,16 @@ module.exports = function ganeshaPlugin (typescript = true) {
 
     name: 'rollup-plugin-ganesha',
 
-    resolveId (source) {
+    resolveId (source, importer) {
+      console.log('resolveId', source, importer)
+      const tsMd = resolve(dirname(importer), `${source}.ts.md`)
+      if (existsSync(tsMd)) {
+        return tsMd
+      }
+      const md = resolve(dirname(importer), `${source}.md`)
+      if (existsSync(md)) {
+        return md
+      }
       return null
     },
 
@@ -20,18 +31,16 @@ module.exports = function ganeshaPlugin (typescript = true) {
       if (isLiterate(id)) {
         code = parseString(code)
         let map = null
-        if (isTypescript(id)){
-          const { transformSync } = require('esbuild')
-          const compiled = transformSync(code, {
-            sourcefile: id,
-            sourcemap: 'both',
-            loader: 'ts',
-            target: 'esnext',
-            format: 'esm' //format === 'module' ? 'esm' : 'cjs'
-          })
-          code = compiled.code
-          map  = compiled.map
-        }
+        const { transformSync } = require('esbuild')
+        const compiled = transformSync(code, {
+          sourcefile: id,
+          sourcemap: 'both',
+          loader: 'ts',
+          target: 'esnext',
+          format: 'esm' //format === 'module' ? 'esm' : 'cjs'
+        })
+        code = compiled.code
+        map  = compiled.map
         return { code, map }
       }
     }
