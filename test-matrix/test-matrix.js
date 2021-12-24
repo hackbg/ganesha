caa83ac1b4e6a4acd2adac410b79b61428e895fa
Adam A <adam@hack.bg>
2021-12-25 00:54:26 +0200

    wip: test: generate some package import cases for test matrix

--- test-matrix/test-matrix.js
+++ test-matrix/test-matrix.js
@@ -60,43 +60,80 @@ const Targets = {
   'CJS in MD' (target) {
     return { 'target.cjs.md': `
       \`\`\`javascript
-      module.exports = require("${target}")
+      module.exports = 123
       \`\`\`
     ` }
   },
   'ESM in MD' (target) {
     return { 'target.esm.md': `
       \`\`\`javascript
-      import * as Target from "${target}"
-      export default Target
+      export default 123
       \`\`\`
     ` }
   },
   'TS in MD'  (target) {
     return { 'target.ts.md': `
       \`\`\`typescript
-      import * as Target from "${target}"
-      export default Target
+      export default 123
       \`\`\`
     ` }
   }
 }
 
 const Environments = {
-  'Node'   () {},
-  'Rollup' () {},
-  'IDE'    () {}
+  'Node'   () {
+  },
+  'Rollup' () {
+  },
+  'IDE'    () {
+  }
 }
 
+const {writeFileSync} = require('fs')
+const {baseName} = require('path')
+const mkdirp = require('mkdirp').sync
+
 const Relations = {
-  'absolute import' () {},
-  'relative import' () {},
-  'package import'  () {}
+
+  'absolute import' (source, target) {
+  },
+
+  'relative import' (source, target) {
+    writeFileSync('package.json', JSON.stringify({
+      "package": "testcase"
+    }), 'utf8')
+    for (const [name, content] of Object.entries(source('./target'))) {
+      writeFileSync(name, content.replace(/\n +/g, '\n'))
+    }
+    for (const [name, content] of Object.entries(target())) {
+      writeFileSync(name, content.replace(/\n +/g, '\n'))
+    }
+  },
+
+  'package import'  (source, target) {
+    mkdirp('source')
+    writeFileSync('source/package.json', JSON.stringify({
+      "name": "source"
+    }), 'utf8')
+    for (const [name, content] of Object.entries(source('target'))) {
+      writeFileSync(`source/${name}`, content.replace(/\n +/g, '\n'))
+    }
+    mkdirp('source/node_modules/target')
+    writeFileSync('source/node_modules/target/package.json', JSON.stringify({
+      "name": "target",
+      "main": Object.entries(target())[0][0]
+    }), 'utf8')
+    for (const [name, content] of Object.entries(target())) {
+      writeFileSync(`source/node_modules/target/${name}`, content.replace(/\n +/g, '\n'))
+    }
+  }
+
 }
 
 process.chdir(__dirname)
-require('mkdirp').sync('snapshots')
-process.chdir('snapshots')
+mkdirp('testcases')
+process.chdir('testcases')
+const testCaseRoot = process.cwd()
 
 for (const environment in Environments) {
   for (const source in Sources) {
@@ -104,7 +141,11 @@ for (const environment in Environments) {
       for (const target in Targets) {
         console.log(`TODO: test that [${source}] can [${relation}] [${target}] in [${environment}]`)
         const testCase = `${environment}/${source} ${relation} ${target}`.toLowerCase().replace(/ /g, '_')
-        require('mkdirp').sync(testCase)
+        mkdirp(testCase)
+        process.chdir(testCase)
+        Environments[environment]()
+        Relations[relation](Sources[source], Targets[target])
+        process.chdir(testCaseRoot)
       }
     }
   }
