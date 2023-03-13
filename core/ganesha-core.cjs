c81727918240bed8fb6d1b379ba793821a140018
Adam A <adam@hack.bg>
2023-03-13 20:06:47 +0200

    refactor(core): use full markdown parser instead of ad-hoc triple-backtick detection

--- core/ganesha-core.cjs
+++ core/ganesha-core.cjs
@@ -33,11 +33,8 @@ const extensions = [
   '.md'
 ]
 
-const RE_NEWLINE = /\r\n|\r|\n/
-
 module.exports = {
   settings,
-  joycon,
   trace,
   parseFile,
   parseString,
@@ -48,57 +45,25 @@ function parseFile (name) {
 }
 
 function parseString (source = "") {
-
-  // The parser works on a line-by-line basis,
-  // so let's start by splitting the source code into lines.
-  const inputLines  = source.split(RE_NEWLINE)
-
-  // Let's collect the lines of the output file here.
-  const outputLines = []
-
-  // Let's start in text mode, and toggle this flag
-  // every time the document switches between prose and code.
-  let isCode = false
-
-  // Let's iterate line by line over the input string,
-  // collecting the processed lines.
-  for (let lineNumber = 0; lineNumber < inputLines.length; lineNumber++) {
-
-    // Trim any spaces from the end of each line.
-    // Used to preserve empty lines between the two modes.
-    let line = inputLines[lineNumber].trimEnd()
-
-    if (line.startsWith('```')) {
-      // Lines that begin with three backticks are
-      // either the beginning or the end of a code block.
-      // We replace those with blank lines.
-      isCode = !isCode
-      outputLines.push(['', true])
-    } else if (line.length === 0) {
-      // Let's keep empty lines empty.
-      // Otherwise empty lines outside code blocks
-      // would get the three-slash prefix.
-      outputLines.push(['', true])
+  let target = ''
+  const tokens = require('marked').lexer(source)
+  for (const token of tokens) {
+    if (token.type === 'code') {
+      target += `\n${token.text}\n`
     } else {
-      // All other lines are pushed as-is to the output buffer,
-      // alongside a flag that determines whether they are code.
-      outputLines.push([line, isCode])
+      const text = (token.raw??'').split('\n').join(`\n/// `)
+      target += text
     }
-
   }
-
-  // The output has the text lines prefixed with three slashes,
-  // the starts/ends of code blocks replaced with empty lines,
-  // and the code front and center.
-  return outputLines.map(([line, isCode])=>isCode?line:`/// ${line}`).join('\n')
-
+  return target
 }
 
 if (require.main === module) {
   const [source] = process.argv.slice(2)
   if (source) {
     try {
-      process.stdout.write(parseFile(source))
+      const target = parseFile(source)
+      process.stdout.write(target)
     } catch (e) {
       process.stderr.write(e.stack)
     }
