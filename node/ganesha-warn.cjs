223fa304edffdd703b35be33132aeffdefa2d8aa
Adam A <adam@hack.bg>
2024-01-13 03:06:22 +0200

    fix(warnings): hide experimental warning

--- node/ganesha-warn.cjs
+++ node/ganesha-warn.cjs
@@ -1,9 +1,24 @@
 // based on https://github.com/nodejs/node/issues/30810
-const {emitWarning} = process
-process.emitWarning = (warning, ...args) => {
-  if (typeof warning === 'string') {
-    if (warning.includes('experimental-loader')) return
-    if (warning.includes('Node.js specifier resolution in ESM is experimental')) return
+// and https://github.com/dword-design/suppress-experimental-warnings
+// (https://github.com/dword-design/suppress-experimental-warnings/blob/master/LICENSE.md)
+
+const {emit, emitWarning} = process
+
+Object.assign(process, {
+
+  emitWarning: (warning, ...args) => {
+    if (typeof warning === 'string') {
+      if (warning.includes('experimental-loader')) return
+      if (warning.includes('Node.js specifier resolution in ESM is experimental')) return
+    }
+    return emitWarning(warning, ...args)
+  },
+
+  emit: function (...args) {
+    if (args[1].name === 'ExperimentalWarning') {
+      return undefined
+    }
+    return emit.call(this, ...args)
   }
-  return emitWarning(warning, ...args)
-}
+
+})
