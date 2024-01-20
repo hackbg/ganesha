c26f2b51349d01372f9c44f48efd39a6cb6096ed
Adam A <adam@hack.bg>
2024-01-21 01:46:59 +0200

    loader: add .ts extension in resolve

--- loader.mjs
+++ loader.mjs
@@ -1,6 +1,6 @@
-import { fileURLToPath } from 'node:url'
+import { fileURLToPath, pathToFileURL } from 'node:url'
 import { extname } from 'node:path'
-import { readFile } from 'node:fs/promises'
+import { readFile, stat } from 'node:fs/promises'
 import { getTsconfig, parseTsconfig } from 'get-tsconfig'
 import * as OXCLoader from './pkg/ganesha_oxc.js'
 
@@ -11,17 +11,33 @@ const typeScriptExtensions = ['.ts', '.mts', '.cts']
 export async function initialize (context) {}
 
 export async function resolve (specifier, context, next) {
-  return next(specifier, context, next)
+  try {
+    return await next(specifier, context, next)
+  } catch (e) {
+    if (specifier.startsWith('./') || specifier.startsWith('../')) {
+      const path = fileURLToPath(new URL(specifier, context?.parentURL).href)
+      const extension = extname(path).toLowerCase()
+      if (!(typeScriptExtensions.includes(extension))) {
+        const extended = path + '.ts'
+        if (await stat(extended)) {
+          return { shortCircuit: true, url: pathToFileURL(extended).href }
+        }
+      }
+    }
+    throw e
+  }
 }
 
 export async function load (url, context, next) {
-  const path = fileURLToPath(url)
-  if (typeScriptExtensions.includes(extname(path).toLowerCase())) {
-    const source = await readFile(path, 'utf8')
-    const config = await readFile(getTsconfig(path).path, 'utf8')
-    const transformed = transformer.transform(path, source, config)
-    return { format: 'module', shortCircuit: true, source: transformed }
-  } else {
-    return next(url, context, next)
+  if (url.startsWith('file://')) {
+    const path = fileURLToPath(url)
+    const extension = extname(path).toLowerCase()
+    if (typeScriptExtensions.includes(extension)) {
+      const source = await readFile(path, 'utf8')
+      const config = await readFile(getTsconfig(path).path, 'utf8')
+      const transformed = transformer.transform(path, source, config)
+      return { format: 'module', shortCircuit: true, source: transformed }
+    }
   }
+  return await next(url, context, next)
 }
