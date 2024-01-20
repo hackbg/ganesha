19df8e2a8f607ea4544498afa1a4c573d28b900b
Adam A <adam@hack.bg>
2024-01-20 21:34:48 +0200

    wip: feat: ts loader

--- loader.mjs
+++ loader.mjs
@@ -1,6 +1,10 @@
 import * as OXCLoader from './pkg/ganesha_oxc.js'
+import { fileURLToPath } from 'node:url'
+import { extname } from 'node:path'
 
 const transformer = new OXCLoader.ModuleTransformer()
+
+const extensions = ['.ts', '.mts', '.cts']
 
 export async function initialize (context) {
   console.log({ context, transformer })
@@ -12,6 +16,18 @@ export async function resolve (specifier, context, next) {
 }
 
 export async function load (url, context, next) {
-  console.log({url, context, next})
-  return next(url, context, next)
+  const path = fileURLToPath(url)
+  const ext = extname(path)
+  if (typescriptExtensions.includes(extname(path))) {
+    return {
+      format: 'module',
+      shortCircuit: true,
+      source: transformer.transform(
+        path,
+        source,
+      )
+    }
+  } else {
+    return next(url, context, next)
+  }
 }
